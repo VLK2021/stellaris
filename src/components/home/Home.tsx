8698198bcfe86fc0953bdd06d51780a076c084e7
vlk2022 <volodymyrkostiuk2021@gmail.com>
2026-06-13 00:15:36 +0300

    ceo

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -3,7 +3,6 @@
 import type {NasaAsset} from "@/src/types/nasa";
 
 import {HeroSection} from "./HeroSection";
-// import {ModuleOrbitRail} from "./ModuleOrbitRail";
 import {TrustCards} from "./TrustCards";
 import {LiveCommandSection} from "@/src/components/home/live-command";
 import {MissionTimelineSection} from "@/src/components/home/mission-timeline";
@@ -17,7 +16,6 @@ export const Home = ({assets}: HomeProps) => {
     return (
         <div className="relative">
             <HeroSection assets={assets} />
-            {/*<ModuleOrbitRail assets={assets} />*/}
             <TrustCards assets={assets} />
             <LiveCommandSection />
             <MissionTimelineSection />
